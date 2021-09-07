b7697f8fd73621a93db50352121d2dc9cb4cdbb9
steven900516 <1569132595@qq.com>
2021-09-07 19:09:45 +0800

    复原IP地址

--- src/RestoreIpAddresses.java
+++ src/RestoreIpAddresses.java
@@ -6,23 +6,47 @@ import java.util.List;
  * @create 2021-09-07
  */
 public class RestoreIpAddresses {
-    ArrayList<String> tot = new ArrayList<>();
+    List<String> ans=new ArrayList<String>();
+
     public List<String> restoreIpAddresses(String s) {
-        backTrack(0,1,s,new StringBuffer());
-        return tot;
+
+        if(s.length()<3 || s.length()>12)
+            return ans;
+        findAns(0,s,0,"");
+
+        return ans;
     }
 
-    private void backTrack(int left,int right,String s,StringBuffer combine){
-        if(right == s.length()){
-            tot.add(combine.toString());
+    private void findAns(int num,String s,int p,String res)
+    {
+        if(num>3) return;
+        if(num==3 && p==s.length())
+        {
+            ans.add(res);
             return;
         }
+        int sum=0;
+        for(int i=p;i<s.length();i++)
+        {
+            sum=sum*10+(s.charAt(i)-48);
 
-        String sub = s.substring(left,right);
-        for (int i = 0; i < s.length(); i++) {
+            if(sum>255) return;
 
+            if(p>0)
+                findAns(num+1,s,i+1,res+"."+s.substring(p, i+1));
+            else
+                findAns(num,s,i+1,res+s.substring(p, i+1));
+
+            if(sum==0) break;
         }
+    }
 
+
+
+    public static void main(String[] args) {
+        RestoreIpAddresses restoreIpAddresses = new RestoreIpAddresses();
+        System.out.println(restoreIpAddresses.restoreIpAddresses("25525511135"));
 
     }
+
 }
