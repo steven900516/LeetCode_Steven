b10835e5b6e8ac9bc8a783df775729a1fc45abff
steven900516 <1569132595@qq.com>
2021-09-11 09:58:38 +0800

    第四天剑指

---
+++ src/FindRepeatNumber.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author Steven0516
+ * @create 2021-09-10
+ */
+
+
+//数组中重复的数字
+public class FindRepeatNumber {
+    public int findRepeatNumber(int[] nums) {
+        Arrays.sort(nums);
+        int tmp = nums[0];
+//        [1,2,3,3,4,5,5,6,7]
+        for (int i = 1; i < nums.length; i++) {
+            if(tmp == nums[i]){
+                return tmp;
+            }else{
+                tmp = nums[i];
+            }
+        }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        FindRepeatNumber findRepeatNumber = new FindRepeatNumber();
+        int[] ints = new int[]{1,2,3,3,4,5,5,6,7};
+        System.out.println(findRepeatNumber.findRepeatNumber(ints));
+    }
+}
---
+++ src/SearchR.java
@@ -0,0 +1,32 @@
+/**
+ * @author Steven0516
+ * @create 2021-09-10
+ */
+public class SearchR {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] <= target) i = m + 1;
+            else j = m - 1;
+        }
+        int right = i;
+        // 若数组中无 target ，则提前返回
+        if(j >= 0 && nums[j] != target) return 0;
+        // 搜索左边界 right
+        i = 0; j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] < target) i = m + 1;
+            else j = m - 1;
+        }
+        int left = j;
+        return right - left - 1;
+
+    }
+
+
+
+
+
+}
