a28967a96dcace39b6ff055007b8c18a19f8c515
steven900516 <1569132595@qq.com>
2021-09-15 15:59:35 +0800

    剑指第九天

---
+++ src/MaxValue.java
@@ -0,0 +1,22 @@
+/**
+ * @author Steven0516
+ * @create 2021-09-15
+ */
+//礼物最大值
+
+
+public class MaxValue {
+    public int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                if(i == 0) grid[i][j] += grid[i][j - 1] ;
+                else if(j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
+}
