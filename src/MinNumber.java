eb3c69f449d3301e7c3dd8fb52b49eff8b221eea
steven900516 <1569132595@qq.com>
2021-09-22 15:24:33 +0800

    剑指第16天

---
+++ src/MinNumber.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * @author Steven0516
+ * @create 2021-09-22
+ */
+public class MinNumber {
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
+}
