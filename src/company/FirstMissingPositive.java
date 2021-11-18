466721eeabfca8faa93ad021706006ba4d634c4f
steven900516 <1569132595@qq.com>
2021-11-18 17:22:59 +0800

    100题 + 缺失的第一个正数(更新)

--- src/company/FirstMissingPositive.java
+++ src/company/FirstMissingPositive.java
@@ -6,24 +6,29 @@ package company;
  */
 public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] <= 0) {
-                nums[i] = n + 1;
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                // 满足在指定范围内、并且没有放在正确的位置上，才交换
+                // 例如：数值 3 应该放在索引 2 的位置上
+                swap(nums, nums[i] - 1, i);
             }
         }
-        for (int i = 0; i < n; ++i) {
-            int num = Math.abs(nums[i]);
-            if (num <= n) {
-                nums[num - 1] = -Math.abs(nums[num - 1]);
-            }
-        }
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] > 0) {
+
+        // [1, -1, 3, 4]
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-        return n + 1;
+        // 都正确则返回数组长度 + 1
+        return len + 1;
+    }
 
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
