0c7a759092ac300fbbfb36f40e697baffb4a50f9
steven900516 <1569132595@qq.com>
2022-01-11 00:29:49 +0800

    二叉树3

--- src/Micrsoft/BuildTree.java
+++ src/Micrsoft/BuildTree.java
@@ -10,10 +10,24 @@ import java.util.HashMap;
  */
 public class BuildTree {
     HashMap<Integer,Integer> map = new HashMap<>();
+    int[] postorder;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-
+        this.postorder = postorder;
         for (int i = 0; i < inorder.length; i++) {
-            map.put()
+            map.put(inorder[i],i);
         }
+        return buildHelper(postorder.length - 1,0,postorder.length);
+    }
+
+    private TreeNode buildHelper(int root, int left, int right) {
+        if(left > right){
+            return null;
+        }
+
+        TreeNode node = new TreeNode(postorder[root]);
+        int i = map.get(postorder[root]);
+        node.left = buildHelper(root - right + i - 1, left, i - 1);
+        node.right = buildHelper(root - 1, i + 1, right);
+        return node;
     }
 }
