a99c30cf6db252e20c845cfab5e0fb55d1c2ef6f
steven900516 <1569132595@qq.com>
2022-04-06 21:25:22 +0800

    二刷剑指

---
+++ src/March/Test.java
@@ -0,0 +1,17 @@
+package March;
+
+/**
+ * @author Steven0516
+ * @create 2022-04-06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Integer a = 100;
+        Integer b = 100;
+        Integer c = 150;
+        Integer d = 150;
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+}
